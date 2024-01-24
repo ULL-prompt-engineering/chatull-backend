d1d1d0fd95024d20a04c1f35de804fb353e83a8b
Gabriel Jonay Vera Estévez <alu0101398198@ull.edu.es>
2024-01-24 16:46:39 +0000

    Session Token management

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ from answer_helper import answer_question, save_time_to_csv
 from regulation import regulation, regulation_sections, regulation_promp_classify, regulation_promp_question
 import uuid
 
+import threading
+import time
+
 documents = buildSections(subjects, sections)
 regulation_docs = buildSections(regulation, regulation_sections)
 
@@ -15,6 +18,14 @@ app = Flask(__name__)
 
 CORS(app)
 
+def print_api_keys():
+    while True:
+        print(time.strftime("%H:%M:%S", time.localtime()), api_keys)
+        time.sleep(15)
+
+t = threading.Thread(target=print_api_keys)
+t.start()
+
 @app.route('/set_api_key', methods=['POST'])
 def set_api_key():
     # Extraer la API key y el token de sesión desde la solicitud
@@ -22,10 +33,10 @@ def set_api_key():
 
     # Establecer la cookie en la respuesta
     if api_key is None:
-        return make_response("API key no encontrada", 401)
+        return make_response("API key no encontrada", 400)
     
     if api_keys.get(api_key) is not None:
-        return make_response("API key ya registrada", 401)
+        return make_response("API key ya registrada", 409)
     
     session_token = str(uuid.uuid4())
 
@@ -35,6 +46,19 @@ def set_api_key():
     api_keys[session_token] = {"api_key": api_key, "ip": request_ip}
 
     response = make_response(jsonify({'message': 'API key guardada exitosamente', 'session_token': session_token}), 200)
+    return response
+
+@app.route('/end_session/<session_token>', methods=['DELETE'])
+def end_session(session_token):
+    session = api_keys.get(session_token)
+    if session is None:
+        return make_response("Sesión no encontrada", 404)
+    
+    if session.get("ip") != request.remote_addr:
+        return make_response("Datos de sesión inválidos", 401)
+    
+    api_keys.pop(session_token)
+    response = make_response(jsonify({'message': 'Sesión finalizada exitosamente'}), 200)
     return response
 @app.route('/get_answer/<session_token>', methods=['GET'])
 def get_answer(session_token):
@@ -95,4 +119,3 @@ def get_documents():
     
 if __name__ == '__main__':
     app.run(debug=True)
-
